6febaeec84d466afb27772cd9f328a3c8823dada
James Carignan <james@carignancreative.com>
2018-10-21 23:20:45 -0600

    Add lambda permission, fix some other resources

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -1,25 +1,27 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_api_gateway_rest_api" "api" {
   name        = "${var.project_name}-api-gateway"
   description = "API Gateway for ${var.project_name}"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "resource" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "${var.project_name}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -33,4 +35,13 @@ resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "test"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.lambda_arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
